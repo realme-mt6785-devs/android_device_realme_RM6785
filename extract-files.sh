7cbd13ba1363060cf984335c92ae62a0cf152e59
SamarV-121 <samarvispute121@gmail.com>
2021-01-31 13:01:07 +0530

    mt6785-common: Update blobs from RMX2001_11_C.01
    Change-Id: I50452d0d08e4c33552dc0d995e184929818a54e6
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- extract-files.sh
+++ extract-files.sh
@@ -34,6 +34,14 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
+function blob_fixup {
+    case "$1" in
+        system_ext/etc/permissions/com.android.hotwordenrollment.common.util.xml)
+            sed -i 's/my_product/system\/system_ext/' "$2"
+            ;;
+    esac
+}
+
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
 
