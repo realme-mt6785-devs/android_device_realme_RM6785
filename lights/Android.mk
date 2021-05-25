947336b798cfaae4cb0f0cd06de2829487f50c89
SamarV-121 <samarvispute121@gmail.com>
2021-05-25 12:18:38 +0530

    mt6785-common: lights: Override stock light AIDL
    Change-Id: I77419259eff6775f0a17b098c09b74635adb1bbb
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- lights/Android.mk
+++ lights/Android.mk
@@ -2,10 +2,11 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := android.hardware.light@2.0-service.realme_mt6785
-LOCAL_MODULE_STEM := android.hardware.light@2.0-service-mediatek
+LOCAL_MODULE_STEM := android.hardware.lights-service.mediatek
 LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PLATFORM_VNDK_VERSION)/bin
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_TAGS  := optional
+LOCAL_REQUIRED_MODULES := lights-mtk-default.xml
 
 LOCAL_SRC_FILES := \
     service.cpp \
@@ -22,3 +23,11 @@ LOCAL_STATIC_LIBRARIES := \
     libutils
 
 include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := lights-mtk-default.xml
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PLATFORM_VNDK_VERSION)/etc/vintf/manifest
+include $(BUILD_PREBUILT)
