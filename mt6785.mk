6cd2a95464004ad82bccce2c85766ba3087326d1
SamarV-121 <samarvispute121@pm.me>
2021-10-30 13:36:51 +0530

    mt6785-common: Compile audio HAL from source
    Change-Id: I3f2be966745911ecfca424c005b8a6d3d7abb753
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- mt6785.mk
+++ mt6785.mk
@@ -40,6 +40,8 @@ TARGET_SCREEN_WIDTH := 1080
 TARGET_EXCLUDES_AUDIOFX := true
 
 PRODUCT_PACKAGES += \
+    android.hardware.audio.service \
+    android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.a2dp.default \
