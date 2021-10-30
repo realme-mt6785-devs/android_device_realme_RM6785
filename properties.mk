b0ffed9e0d08ce07487b315c15e8a399ccf826c6
SamarV-121 <samarvispute121@pm.me>
2021-10-30 13:34:41 +0530

    mt6785-common: Cleanup props
    Change-Id: Icb54a69c4b7ed24791610a9c6b1652563dc01cfe
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- properties.mk
+++ properties.mk
@@ -16,31 +16,11 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Dex2oat
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    dalvik.vm.dex2oat64.enabled=true \
-    dalvik.vm.image-dex2oat-filter=quicken \
-    dalvik.vm.image-dex2oat-threads=8 \
-    dalvik.vm.image-dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
-    dalvik.vm.dex2oat-filter=quicken \
-    dalvik.vm.dex2oat-threads=8 \
-    dalvik.vm.dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
-    dalvik.vm.boot-dex2oat-threads=8 \
-    dalvik.vm.boot-dex2oat-cpu-set=0,1,2,3,4,5,6,7
+    dalvik.vm.dex2oat64.enabled=true
 
 # Display
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.sf.lcd_density=420
-
-# IMS
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    persist.dbg.volte_avail_ovr=1 \
-    persist.dbg.vt_avail_ovr=1 \
-    persist.dbg.wfc_avail_ovr=1
-
-# LMKD
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.lmk.psi_complete_stall_ms=150 \
-    ro.lmk.swap_free_low_percentage=20 \
-    ro.lmk.kill_timeout_ms=100
 
 # NFC
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
@@ -49,6 +29,12 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.sys.nfc.aid_overflow=true \
     ro.product.cuptsm=OPPO|ESE|01|02 \
     persist.sys.nfc.antenna_area=bottom
+
+# IMS
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1
 
 # OMX
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
@@ -65,16 +51,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # Surfaceflinger
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     debug.sf.latch_unsignaled=1 \
-    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
-    ro.surface_flinger.running_without_sync_framework=true \
-    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
-    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
-
-# ZRAM
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.zram.mark_idle_delay_mins=60 \
-    ro.zram.first_wb_delay_mins=1440 \
-    ro.zram.periodic_wb_delay_hours=24
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3
 
 # Zygote
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
