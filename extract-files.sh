8c9dc7a1d0db3b8cb4204689a7fc1e2475990278
SamarV-121 <samarvispute121@pm.me>
2021-11-01 15:53:52 +0530

    mt6785-common: Use libutils-v30 for libmtkcam_stdutils.so
    `F linker  : CANNOT LINK EXECUTABLE "/odm/bin/hw/camerasloganserver": cannot locate symbol "_ZN7android11PropertyMapD1Ev" referenced by "/vendor/lib64/libmtkcam_stdutils.so"`
    
    Change-Id: I6e5a7691c951003949ed497c91b94ccfaeed45e0
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- extract-files.sh
+++ extract-files.sh
@@ -45,6 +45,9 @@ function blob_fixup {
         vendor/lib*/libudf.so)
             "$PATCHELF" --replace-needed "libunwindstack.so" "libunwindstack-v30.so" "$2"
             ;;
+        vendor/lib*/libmtkcam_stdutils.so)
+            "$PATCHELF" --replace-needed "libutils.so" "libutils-v30.so" "$2"
+            ;;
     esac
 }
 
