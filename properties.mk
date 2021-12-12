315bea20a33c99d796f14249b29c93a439a47393
SamarV-121 <samarvispute121@pm.me>
2021-12-12 12:55:44 +0530

    RM6785: Move surfaceflinger props to vendor
    Change-Id: I69c1ee1a6fd42219210d5922c8f0a7c6f4788202
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- properties.mk
+++ properties.mk
@@ -28,11 +28,6 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.mtk_perf_fast_start_win=1 \
     ro.mtk_perf_response_time=1
 
-# Surfaceflinger
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    debug.sf.latch_unsignaled=1 \
-    ro.surface_flinger.max_frame_buffer_acquired_buffers=3
-
 # Zygote
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.device_config.runtime_native.usap_pool_enabled=true
