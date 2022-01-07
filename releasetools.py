717339acc07a121724865e2ae339d73f08833463
SamarV-121 <samarvispute121@pm.me>
2022-01-07 14:30:20 +0530

    Revert "RM6785: Compile dtbo from source"
     * Touch dead on RMX2151
    
    This reverts commit da3db4cf9843fb3e12e60611d869819e99a51651.

--- releasetools.py
+++ releasetools.py
@@ -39,7 +39,6 @@ def AddImage(info, basename, dest, incremental):
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
 
 def OTA_InstallEnd(info, incremental):
-  AddImage(info, "dtbo.img", "/dev/block/platform/bootdevice/by-name/dtbo", incremental)
   AddImage(info, "vbmeta.img", "/dev/block/platform/bootdevice/by-name/vbmeta", incremental)
 
   bin_map = {
@@ -51,6 +50,7 @@ def OTA_InstallEnd(info, incremental):
       'cam_vpu1': ['cam_vpu1'],
       'cam_vpu2': ['cam_vpu2'],
       'cam_vpu3': ['cam_vpu3'],
+      'dtbo': ['dtbo'],
       'gz': ['gz1', 'gz2'],
       'lk': ['lk', 'lk2'],
       'md1img': ['md1img'],
