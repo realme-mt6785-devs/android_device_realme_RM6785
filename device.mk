5bf033bebe00654901221c5486e6e74124fc0609
Bruno Martins <bgcngm@gmail.com>
2022-03-15 20:53:47 +0000

    RM6785: Copy cgroups and task_profiles directly from system/core
    This ensures that the most up-to-date JSON files are shipped.
    
    Change-Id: Ic9fae188784825ae6dc607c13b403e37290b7067
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -58,6 +58,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     disable_configstore
 
+# Cgroup and task_profiles
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Doze
 PRODUCT_PACKAGES += \
     OplusDoze
