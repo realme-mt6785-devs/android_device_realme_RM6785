0c9f2115052e342c65c4bebda2ff53dd420a0b11
SamarV-121 <samarvispute121@pm.me>
2022-04-13 10:25:39 +0530

    RM6785: rootdir: Switch to blueprint
    Change-Id: I22e38733e72bfadaf35c0bb80b260d89056422a5
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -17,36 +17,10 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.target.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.target.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.mt6785
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/fstab.mt6785
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/etc
-LOCAL_REQUIRED_MODULES := fstab.ramdisk
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.ramdisk
 LOCAL_MODULE_STEM  := fstab.mt6785
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.mt6785
 LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := ueventd.oplus.rc
-LOCAL_MODULE_STEM  := ueventd.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/ueventd.oplus.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ODM_ETC)
 include $(BUILD_PREBUILT)
