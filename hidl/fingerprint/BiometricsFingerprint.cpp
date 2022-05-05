027b8a618a61f0a82b437a6e856d7e2127e0e52d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-05 15:53:23 +0200

    RM6785: fingerprint: Drop LOG_VERBOSE
    * It's useless since it's the same as LOG_TAG
    
    Change-Id: I1a15ea85f840b7af6d41b59cf01b0ec3c2457705
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- hidl/fingerprint/BiometricsFingerprint.cpp
+++ hidl/fingerprint/BiometricsFingerprint.cpp
@@ -4,7 +4,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 #define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service.RM6785"
-#define LOG_VERBOSE "android.hardware.biometrics.fingerprint@2.1-service.RM6785"
 
 #include <hardware/hardware.h>
 #include <hardware/fingerprint.h>
