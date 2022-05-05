3bde0afd376d5609e1dc558c2557ab14ae8190b9
SamarV-121 <samarvispute121@pm.me>
2022-05-05 14:58:55 +0530

    RM6785: Initial powerhint for power-libperfmgr
     * Based on https://github.com/realme-mt6785-devs/android_device_realme_RM6785/blob/1fad0c23a810e24f2de87763de094891897e5f4c/configs/power/powerscntbl.xml
    
    Change-Id: I093b9277e9bfbf26781fbfece8ed39e33b142c92
    Signed-off-by: Himanshu Tyagi <himanshut567@gmail.com>

--- device.mk
+++ device.mk
@@ -245,6 +245,9 @@ PRODUCT_PACKAGES += \
     vendor.mediatek.hardware.mtkpower@1.1.vendor \
     vendor.mediatek.hardware.mtkpower@1.2-service.stub
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Properties
 -include $(LOCAL_PATH)/properties.mk
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
