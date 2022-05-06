0d78b0327e82ca090fc5a394d207c1c95ef8afae
SamarV-121 <samarvispute121@pm.me>
2022-05-06 18:48:30 +0530

    RM6785: Drop redundant secure element service
    Change-Id: I42311c83d699bc346d156317b0a202e30347a34b
    Signed-off-by: SamarV-121 <samarvispute121@pm.me>

--- device.mk
+++ device.mk
@@ -165,8 +165,6 @@ PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service \
     com.android.nfc_extras \
     NfcNci \
-    android.hardware.secure_element@1.0-service-disabled \
-    SecureElement \
     Tag
 
 PRODUCT_COPY_FILES += \
