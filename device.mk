93f2be61af00cd4ae46c599343b2b2530370b76b
SamarV-121 <samarvispute121@pm.me>
2022-08-19 20:41:22 +0530

    RM6785: Move HotwordEnrollment blobs to product
    Change-Id: I6a564d51e7ec01868d964d69379c151f0a39ad96

--- device.mk
+++ device.mk
@@ -183,7 +183,7 @@ PRODUCT_ENFORCE_RRO_TARGETS += *
 # Permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml \
-    $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-hotword.xml \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
--- extract-files.sh
+++ extract-files.sh
@@ -39,8 +39,8 @@ function blob_fixup {
         lib/libsink.so)
             "$PATCHELF" --add-needed "libshim_vtservice.so" "$2"
             ;;
-        system_ext/etc/permissions/com.android.hotwordenrollment.common.util.xml)
-            sed -i 's/my_product/system_ext/' "$2"
+        product/etc/permissions/com.android.hotwordenrollment.common.util.xml)
+            sed -i 's/my_product/product/' "$2"
             ;;
         vendor/etc/init/hw/*.rc)
             sed -i 's ${ro.vendor.rc} /vendor/etc/init/hw/ g' "$2"
