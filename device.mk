e0e52a82f82c01b54079650759e238aa1f7c34cc
SamarV-121 <samarvispute121@pm.me>
2022-08-31 15:08:35 +0530

    RM6785: sepolicy: Label and compile health AIDL
    Change-Id: I643ae8a4a0e87621105a91f08030b2a6b8845ef6

--- device.mk
+++ device.mk
@@ -111,8 +111,8 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
+    android.hardware.health-service.RM6785 \
+    android.hardware.health-service.RM6785-recovery
 
 # HIDL
 PRODUCT_PACKAGES += \
