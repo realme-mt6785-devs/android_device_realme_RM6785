af04798d72698f27ca90003e25d59bfe42021e09
SamarV-121 <samarvispute121@pm.me>
2022-09-14 10:57:58 +0530

    Revert "RM6785: sepolicy: Allow charger to write backlight nodes"
    This reverts commit 01402902712bd07fd5b901f3d82b5a2f969c4c30.
    
    Change-Id: I87ae7e72fb2472957c99039da33ee4cc1c953737

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -146,7 +146,6 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # Sepolicy
 include device/mediatek/sepolicy/sepolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # SPL
