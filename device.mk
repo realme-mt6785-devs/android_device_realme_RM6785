2e86f79926850d1fa06e84db1f9bc22ca2610dba
Tim Zimmermann <tim@linux4.de>
2022-09-21 15:17:12 +0530

    RM6785: Switch to new AIDL memtrack HAL
    Change-Id: I3ae077b5f59e4fcf32489eef64bce8551d758998

--- device.mk
+++ device.mk
@@ -158,8 +158,7 @@ PRODUCT_COPY_FILES += \
 
 # Memtrack
 PRODUCT_PACKAGES += \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service
+    android.hardware.memtrack-service.RM6785
 
 # Neutral Networks
 PRODUCT_PACKAGES += \
