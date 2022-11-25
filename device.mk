5387b26ed91bf57c74ffda6de7097d716feb4977
SamarV-121 <samarvispute121@pm.me>
2022-11-25 15:00:50 +0530

    RM6785: Don't include dev gsi pubkeys
     * We can't boot shit in locked state
    
    Change-Id: I86e3d674157ec976bd507b77d43d359249d2aee9

--- device.mk
+++ device.mk
@@ -14,9 +14,6 @@
 # limitations under the License.
 #
 
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
-
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
