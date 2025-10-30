fc2ec22b11e1c787ba13ccbbfb723fb6535b428b
Naveen <133593113+elohim-etz@users.noreply.github.com>
2025-10-30 11:52:46 +0530

    Revert "RM6785: Include Viper4Android"
    This reverts commit dd2b684b0d5f01fd427a2edf667f5745423639db.

--- device.mk
+++ device.mk
@@ -7,9 +7,6 @@
 # Call proprietary blob setup
 $(call inherit-product, vendor/realme/RM6785/RM6785-vendor.mk)
 
-# V4A
-$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
-
 # Shipping API
 PRODUCT_SHIPPING_API_LEVEL := 29
 
