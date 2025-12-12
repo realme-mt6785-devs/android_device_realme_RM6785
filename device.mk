935e5e8ad2ece2a9d354593f318f6a688e350ae3
0xCAFEBABE <me.cafebabe@gmail.com>
2025-12-12 17:23:18 +0900

    RM6785: Update some soong config variables to bool type
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -45,7 +45,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
-$(call soong_config_set,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
+$(call soong_config_set_bool,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
 
 $(call inherit-product, hardware/dolby/dolby.mk)
 
