efdb426ffce16c28ce2e4d17eee2075e66fde755
basamaryan <basam.aryan@gmail.com>
2025-12-14 18:59:05 -0800

    RM6785: overlay: Disable blurs by default
    Change-Id: Ia323bb8f66e3515bdf16ff3a14c919b8bd859eba
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -203,6 +203,7 @@ PRODUCT_PACKAGES += \
     FrameworksResOverlayRM6785 \
     LauncherOverlayRM6785 \
     LineageSDKOverlayRM6785 \
+    LineageSettingsProviderOverlayRM6785 \
     SettingsOverlayRM6785 \
     SettingsProviderOverlay6 \
     SettingsProviderOverlay6i \
