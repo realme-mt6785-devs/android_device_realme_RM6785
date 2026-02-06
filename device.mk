b515d697a4a77f1659f649d0e0a0fdd4583e12e1
Naveen <133593113+elohim-etz@users.noreply.github.com>
2026-02-06 10:40:57 +0530

    RM6785: Add support for V4A
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -9,6 +9,8 @@ $(call inherit-product, vendor/realme/RM6785/RM6785-vendor.mk)
 
 # JDSP
 $(call inherit-product, packages/apps/JamesDSPManager/config.mk)
+# V4A
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 
 # Shipping API
 PRODUCT_SHIPPING_API_LEVEL := 29
