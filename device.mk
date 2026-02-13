9a593d2ceab2eae7944294583afcbb15c5ce9529
Debayan Kar <kardebayan3@gmail.com>
2026-02-13 05:24:30 +0000

    RM6785: Bring `hals.conf` into tree
    Signed-off-by: Debayan Kar <kardebayan3@gmail.com>
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -300,6 +300,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.recovery.mt6785.rc
 
+# Sensors
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
