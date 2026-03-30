53a98e429f36f024dbced36ac244d0b37861a7d1
Mashopy <eliasgheeraert@gmail.com>
2026-03-30 03:15:19 +0200

    RM6785: Rename memtrack service to the new impl
    Change-Id: I50e1ac6bda22a4c0895f9fec785b9af1d0f81ad5
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -166,7 +166,7 @@ PRODUCT_COPY_FILES += \
 
 # Memtrack
 PRODUCT_PACKAGES += \
-    android.hardware.memtrack-service.mediatek-mali
+    android.hardware.memtrack-service.mediatek
 
 # MtkInCallService
 PRODUCT_PACKAGES += \
