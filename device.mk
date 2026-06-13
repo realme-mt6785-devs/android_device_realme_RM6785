caf80705503543aa610e644494e2ab68f318f308
Mashopy <eliasgheeraert@gmail.com>
2026-06-13 18:53:52 +0700

    RM6785: Inherit MSSI common overlays
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -169,6 +169,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     MtkInCallService
 
+# Platform
+TARGET_BOARD_PLATFORM := mt6785
+
 # Reduce system server verbosity.
 PRODUCT_SYSTEM_SERVER_DEBUG_INFO := false
 PRODUCT_OTHER_JAVA_DEBUG_INFO := false
@@ -176,6 +179,9 @@ PRODUCT_OTHER_JAVA_DEBUG_INFO := false
 # Remove unwanted packages
 PRODUCT_PACKAGES += \
     RemovePackages
+
+# RIL
+ENABLE_VENDOR_RIL_SERVICE := true
 
 # NFC
 PRODUCT_PACKAGES += \
@@ -190,6 +196,9 @@ PRODUCT_COPY_FILES += \
 # Neural Networks
 PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.3.vendor:64
+
+# Overlays
+$(call inherit-product, hardware/mediatek/overlay/mssi.mk)
 
 PRODUCT_PACKAGES += \
     ApertureOverlayRM6785 \
@@ -209,8 +218,7 @@ PRODUCT_PACKAGES += \
     SettingsProviderOverlayNarzo \
     SettingsProviderOverlayNarzo20Pro \
     SettingsProviderOverlayNarzo30 \
-    SystemUIOverlayRM6785 \
-    TelephonyOverlayRM6785
+    SystemUIOverlayRM6785
 
 # RRO (Runtime Resource Overlay)
 PRODUCT_ENFORCE_RRO_TARGETS += *
@@ -361,8 +369,6 @@ PRODUCT_PACKAGES += \
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
-    TetheringConfigOverlayRM6785 \
-    WifiOverlayRM6785 \
     hostapd \
     wpa_supplicant \
     android.hardware.wifi-service
